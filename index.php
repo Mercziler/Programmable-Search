d4aaa5212a84892beb704cd5dd18d02e5769ac2d
Mercziler <dog.antoine@gmail.com>
2022-07-14 14:10:00 +0000

    Programmble search engine

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html>
+<head>
+<title>my site</title>
+</head>
+<body>
+<div1><h1>SEARCH ENGINE</h1></div1>
+	<script async src="https://cse.google.com/cse.js?cx=a2193bf7d2e3e550e"></script>
+<div class="gcse-search"></div>
+</body>
+</html>
